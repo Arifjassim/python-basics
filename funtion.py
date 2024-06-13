42ca7fb0c9629235db2923b11ae6c3421917611a
arifjazzim <jassimarif71@gmail.com>
2024-06-13 11:27:56 +0530

    function

---
+++ funtion.py
@@ -0,0 +1,56 @@
+def my_name():
+    print("hello")
+my_name()
+
+
+def names(fname,lname):
+    print(fname+" "+lname)
+names("jassim","arif")
+
+
+def arbitrary(*kids):
+    print(kids[2])
+arbitrary("tom","jerry","oggy","jack")
+arbitrary("a","b","c","d","e")
+
+
+def keyword(a,b,c):
+    print(b)
+keyword(a=1,b=2,c=3)
+
+
+def arbitrary_keyword(**kids):
+    print(kids["b"])
+arbitrary_keyword(a=0,b=8,c=7)
+fruits=["mango","grape","apple","orange"]
+
+
+def frui(foods):
+    for i in foods:
+     print(i) 
+frui(fruits)  
+
+
+d=int(input("enter a number"))
+def number(f):
+    if (f%2)==0:
+        print("even")
+    else:
+        print("odd")
+number(d)  
+        
+
+f=int(input("enter a number"))
+def number(s):
+    if(s>1):
+        for i in range(2,s):
+            if (s%i)==0:
+                print("not a prime")
+                break
+        else:
+                print("prime number")
+    else:
+        print("not a prime")
+number(f)
+
+
